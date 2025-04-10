c4b90f175ba42e899f2c428aa91da11d6a1dc5b8
Conti, Geovanni [ITSBR Non-J&J] <gconti@its.jnj.com>
2025-04-10 15:50:32 -0300

    Add functions for demo purposes

---
+++ functions/users.js
@@ -0,0 +1,9 @@
+export default function handler(request, response) {
+  const users = [
+    {name: "Jack", age: "25"},
+    {name: "Rick", age: "28"},
+    {name: "Jane", age: "34"},
+  ];
+  response.status(200).send(users);
+}
+
